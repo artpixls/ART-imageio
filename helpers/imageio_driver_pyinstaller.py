dfcfd35ab40090761b2b11aecd86a710a68ab85c
Alberto Griggio <griggio@fbk.eu>
2024-09-26 13:51:08 +0200

    added helpers to generate a windows binary installation

---
+++ helpers/imageio_driver_pyinstaller.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+"""
+Script to generate a binary installation for Windows of the ART-imageio scripts
+using PyInstaller.
+(Note: this doesn't collect the external binary dependencies
+needed to run the scripts)
+"""
+
+import os, sys, argparse
+import PyInstaller.__main__
+
+
+def getopts():
+    p = argparse.ArgumentParser()
+    p.add_argument('-o', '--outdir', required=True, help='output directory')
+    ret = p.parse_args()
+    ret.imageio_dir = os.path.normpath(os.path.abspath(ret.imageio_dir))
+    return ret
+
+
+def main():
+    mydir = os.path.abspath(os.path.dirname(__file__))
+    imageio_dir = os.path.abspath(os.path.join(mydir), '..')
+    opts = getopts()
+    hidden_imports = []
+    for (dirpath, dirnames, filenames) in os.walk(imageio_dir):
+        if dirpath != mydir:
+            for name in filenames:
+                if name.endswith('.py'):
+                    sys.path.append(dirpath)
+                    m = os.path.splitext(name)[0]
+                    hidden_imports.append('--hidden-import=' + m)
+        else:
+            dirnames[:] = []
+                
+    os.chdir(opts.outdir)
+    print(';; directory is: %s' % os.getcwd())
+
+    sep = os.pathsep
+    tool = os.path.join(mydir, 'imageio_driver.py')
+
+    args = ['--name=python', '--clean'] + hidden_imports + [tool]
+    PyInstaller.__main__.run(args)    
+
+
+if __name__ == '__main__':
+    main()
